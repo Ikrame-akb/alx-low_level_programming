948772d4e446ed9054671e34f3a362624b570478
Ikrame-akb <ikrame.akabli@gmail.com>
2023-03-21 01:50:52 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,5 @@ int main(void)
 	printf("Size of a long int: %zu	byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)", sizeof(float));
-	return 0;
+	return (0);
 }
