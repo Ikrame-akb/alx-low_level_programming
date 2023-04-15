f1ba7fa43631c9cf876c0562dba3f9eaa1b8e0fe
Ikrame-akb <ikrame.akabli@gmail.com>
2023-04-15 00:34:40 +0000

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
+		return (0);
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
