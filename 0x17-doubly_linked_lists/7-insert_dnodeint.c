2ca8be74789b7b981b145df238dfa3b3cc0c9a08
Ikrame-akb <ikrame.akabli@gmail.com>
2023-06-16 23:47:15 +0000

    C insert dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int position = 0;
 
 	if (idx == 0)
-		return add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 
 	while (position < idx - 1 && current != NULL)
 	{
